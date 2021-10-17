307363667d8128ac9b109b1fede139bea794ac43
Christian Nunciato <c@nunciato.org>
2021-10-17 08:50:43 -0700

    Remove unused environment variables

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -7,17 +7,6 @@ source ./scripts/common.sh
 # URL to the Pulumi conversion service.
 export PULUMI_CONVERT_URL="${PULUMI_CONVERT_URL:-$(pulumi stack output --stack pulumi/tf2pulumi-service/production url)}"
 
-printf "Compiling theme JavaScript and CSS...\n\n"
-export ASSET_BUNDLE_ID="$(build_identifier)"
-
-# Paths to the CSS and JS bundles we'll generate below. Note that environment variables
-# are read by some templates during the Hugo build process.
-export CSS_BUNDLE="static/css/styles.${ASSET_BUNDLE_ID}.css"
-export JS_BUNDLE="static/js/bundle.min.${ASSET_BUNDLE_ID}.js"
-
-# Relative paths to those same files, read by Hugo templates.
-export REL_CSS_BUNDLE="/css/styles.${ASSET_BUNDLE_ID}.css"
-export REL_JS_BUNDLE="/js/bundle.min.${ASSET_BUNDLE_ID}.js"
 export REPO_THEME_PATH="themes/default/"
 
 printf "Copying prebuilt docs...\n\n"
